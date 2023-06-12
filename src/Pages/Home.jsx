8a2c9b290f8ff377f2aff6d631a3292575b2c752
Jorge Cortes <77340382+chochelius@users.noreply.github.com>
2023-06-11 21:33:39 -0400

    finalfinal

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className="home w-100 h-100">
+        <div className="home ">
             <div className="row">
 
                 <div className="col-sm-6">
@@ -11,7 +11,7 @@ const Home = () => {
                     />
 
                 </div>
-                <div className="col-sm-6 align-content-center mt-5 p-5">
+                <div className="col-sm-6 pt-5 align-content-center justify-content-around mt-5 text-start mb-5">
 
                     <p className="fs-4 text-white">Bienvenido a la galería de imagenes. <br /> Aquí podrás ver muchos  paisajes </p> <br /> <p className=" fs-4 text-white text-decoration-underline">  Ninguno de estos paisajes es real </p> <br /> <p className=" font-monospace text-white"> Fueron hechos con inteligencia artificial generativa </p> <br /> <p className="text-white fs-4"> Espero que te gusten</p>
                 </div>
